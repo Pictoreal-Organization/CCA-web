f13b053beb45a0deadf5432d8f2d530565916a83
riddhilahare14 <lahare.riddhi14@gmail.com>
2025-11-01 22:42:55 +0530

    minor change

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "438010710"
+    serviceWorkerVersion: "1079855185"
   }
 });
